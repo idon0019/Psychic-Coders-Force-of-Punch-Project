7cbbf496ba3d9e4303388f22397d18a070fc8e0f
zetamine <xiaofanyi15@gmail.com>
2021-01-19 18:55:15 -0500

    Data point tap text finished.

--- app/src/main/java/com/example/ui_test/MainActivity.java
+++ app/src/main/java/com/example/ui_test/MainActivity.java
@@ -4,79 +4,93 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.graphview.series.OnDataPointTapListener;
-import com.jjoe64.graphview.series.Series;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Date date;
-    private Calendar time;
+    GraphView graph;
+    TextView infoText;
+    final String pattern = "####.#";
+    final DecimalFormat format = new DecimalFormat(pattern);
+    final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        time = Calendar.getInstance();
+        Calendar time = Calendar.getInstance();
         Random rand = new Random();
 
+        // links infoText in code to TextView in layout
+        infoText = findViewById(R.id.infoText);
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
+        // Links graph in code to graphView in the layout
+        graph = findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
         graph.addSeries(series);
 
         // touch listener that displays data point.
-        // TODO: format the data points into readable text.
-        series.setOnDataPointTapListener((series1, dataPoint) -> Toast.makeText(getActivity(), "" + dataPoint, Toast.LENGTH_SHORT).show());
+        series.setOnDataPointTapListener((series1, dataPoint) -> updateText(dataPoint));
 
-        time.set(100, 3, 4);
-        date = time.getTime();
+        time.set(90, 3, 4);
+        Date date = time.getTime();
         graph.getViewport().setMinX(date.getTime());
         series.appendData(new DataPoint(date, rand.nextDouble()), true, 100);
 
-        time.set(100, 3, 12);
-        date = time.getTime();
-        series.appendData(new DataPoint(date, rand.nextDouble()), true, 100);
-
+        // creates some data points
         for (int i = 0; i < 40; i++) {
-            time.set(100+i, 3, 12);
+            time.set(90 + i, 3, 12);
             date = time.getTime();
             series.appendData(new DataPoint(date, rand.nextDouble()), true, 100);
         }
 
+        graph.getViewport().setMaxX(date.getTime());
+
         // set date label formatter
         graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
-        //graph.getGridLabelRenderer().setNumHorizontalLabels(4); // only 4 because of the space
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
 
-// activate horizontal zooming and scrolling
+        // activate horizontal zooming and scrolling
         graph.getViewport().setScalable(true);
 
-// activate horizontal scrolling
+        // activate horizontal scrolling
         graph.getViewport().setScrollable(true);
 
-// activate horizontal and vertical zooming and scrolling
+        // activate horizontal and vertical zooming and scrolling
         graph.getViewport().setScalableY(true);
 
-// activate vertical scrolling
+        // activate vertical scrolling
         graph.getViewport().setScrollableY(true);
 
-// as we use dates as labels, the human rounding to nice readable numbers
-// is not necessary
+        // as we use dates as labels, the human rounding to nice readable numbers
+        // is not necessary
         graph.getGridLabelRenderer().setHumanRounding(false);
     }
 
     private Context getActivity() {
         return this;
+    }
+
+    // sets the infoView text when a datapoint is tapped.
+    private void updateText(DataPointInterface dataPoint) {
+        Date data = new Date((long) dataPoint.getX());
+        String force = this.format.format(dataPoint.getY());
+
+        String text = "Date: "+this.dateFormat.format(data)+"\nForce Value: "+force;
+
+        this.infoText.setText(text);
     }
 }
