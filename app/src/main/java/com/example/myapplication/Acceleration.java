2cc0bdddce3f54ae43c4cc43800fa1695bcd670e
returnoftheg <52305493+returnoftheg@users.noreply.github.com>
2021-02-17 17:25:53 -0500

    punch display from sensors

---
+++ app/src/main/java/com/example/myapplication/Acceleration.java
@@ -0,0 +1,82 @@
+package com.example.myapplication;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+/*import android.widget.TextView;*/
+/*import java.text.DecimalFormat;*/
+
+
+public class Acceleration implements SensorEventListener {
+    private Double maxAcceleration = 0.0;
+    private double acceleration = 0;
+    private SensorManager senManager;
+    private Sensor sen;
+/*    private TextView accelView; // TextView to display the current acceleration
+    private TextView maxAccelView; // TextView to display the max acceleration
+    String pattern = "##.#"; // Used to format the force
+    DecimalFormat format = new DecimalFormat(pattern);*/
+
+    public Acceleration(SensorManager manager/*, TextView aView, TextView mAView*/) {
+        this.senManager = manager;
+        assert senManager != null;
+
+        //creates variable that gets data from the linear accelerometer
+        sen = senManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        //registers a listener for sen
+        senManager.registerListener(this, sen, SensorManager.SENSOR_DELAY_NORMAL);
+        /*this.accelView = aView;
+        this.maxAccelView = mAView;*/
+    }
+
+    /**
+     * Triggers when the sensors returns a value (which is at a set interval)
+     * @param event
+     */
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Double linAcceleration;
+
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        linAcceleration = Math.sqrt(x * x + y * y + z * z); //gets the total linear acceleration from each axis
+        this.acceleration = linAcceleration;
+
+        // updates to a new maxAcceleration is newer value is larger
+        // also updates the view if maxAcceleration changed
+        if (this.maxAcceleration < linAcceleration) {
+            this.maxAcceleration = linAcceleration;
+//            this.maxAccelView.setText("Max acceleration: " + format.format(this.maxAcceleration));
+        }
+
+        /*// displays the current linear acceleration
+        this.accelView.setText(format.format(linAcceleration));
+        this.accelView.setText("Current Acceleration: "+ this.accelView.getText());*/
+    }
+
+
+    public double calculateForce() {
+        double bagMass;
+        double force;
+        bagMass = 40;/*arbitrary number, macro implementation possible*/
+        force = bagMass * this.maxAcceleration;
+        return acceleration;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // ignore
+    }
+
+    public double getMaxAcceleration() {
+        return this.maxAcceleration;
+    }
+
+    public void resetMaxAcceleration() {
+        this.maxAcceleration = 0.0;
+    }
+}
--- app/src/main/java/com/example/myapplication/PunchResultFragment.java
+++ app/src/main/java/com/example/myapplication/PunchResultFragment.java
@@ -1,22 +1,41 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.myapplication.DataModel.PunchModel;
+
+import java.util.List;
 
 public class PunchResultFragment extends Fragment {
 
     private Button btnTryAgain, btnRecord;
     private NavController navController;
+    private TextView txtPunchResult;
+    private String punchString;
+    private SensorManager senManager;
+    private double punchScore;
+
+
 
     public PunchResultFragment() {
         // Required empty public constructor
@@ -37,6 +56,25 @@ public class PunchResultFragment extends Fragment {
 
         btnTryAgain = view.findViewById(R.id.BtnTryAgain);
         btnRecord = view.findViewById(R.id.BtnRecord);
+
+        txtPunchResult = view.findViewById(R.id.TxtPunchForceResult);
+        Acceleration punchSensor = new Acceleration(senManager);
+        punchScore = punchSensor.calculateForce();
+
+        Bundle bundle = new Bundle();
+        bundle.putDouble("punchScore", punchScore);
+        getParentFragmentManager().setFragmentResult("punchScore", bundle);
+
+
+        getParentFragmentManager().setFragmentResultListener("punchScore", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                punchScore = result.getDouble("punchScore");
+                punchString = punchScore + " N";
+                txtPunchResult.setText(punchString);
+            }
+        });
+
 
         btnTryAgain.setOnClickListener(new View.OnClickListener() {
             @Override
